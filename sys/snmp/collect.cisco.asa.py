44509dc1be743be35ce6fd00784959ab4ca19c62
karl <karl.rink@nationsinfocorp.com>
2020-03-30 14:30:32 -0700

    collect.cisco.asa config.py

--- sys/snmp/collect.cisco.asa.py
+++ sys/snmp/collect.cisco.asa.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python2
 
 import netsnmp
+import json
+import config
+
+system_id = config.param['system_id']
+dest_host = config.param['dest_host']
 
 oid = netsnmp.VarList('IF-MIB::ifName','IF-MIB::ifOperStatus', 
         'IF-MIB::ifInOctets','IF-MIB::ifInUcastPkts','IF-MIB::ifInNUcastPkts',
@@ -10,32 +15,67 @@ oid = netsnmp.VarList('IF-MIB::ifName','IF-MIB::ifOperStatus',
         'IF-MIB::ifInMulticastPkts','IF-MIB::ifInBroadcastPkts',
         'IF-MIB::ifOutMulticastPkts','IF-MIB::ifOutBroadcastPkts')
 
-#oid = netsnmp.VarList('IF-MIB::ifName','IF-MIB::ifOperStatus')
-
-snmp_res = netsnmp.snmpwalk(oid, Version=1, DestHost='216.34.200.142', Community='public')
-#for x in range(2):
-#    #print "snmp_res:: ", x.iid, " = ", x.val
-#    print(ifName.val)
-#    print(ifOperStatus.val)
-#for x in oid:
-#print(snmp_res)
-
-#print(type(snmp_res))
-#print(snmp_res)
+snmp_res = netsnmp.snmpwalk(oid, Version=1, DestHost=str(dest_host), Community='public')
 
 def chunks(l, n):
     for i in range(0, len(l), n):
         yield l[i:i+n]
 
-#returnList = list(chunks(snmp_res, 16))
 returnList = chunks(snmp_res, 16)
 
+rrDict = {}
+rrList = []
 for i in returnList:
-    print(i)
+    #print(i)
+    ifName = str(i[0])
+    ifOperStatus = str(i[1])
+    ifInOctets = str(i[2])
+    ifInUcastPkts = str(i[3])
+    ifInNUcastPkts = str(i[4])
+    ifInDiscards = str(i[5])
+    ifInErrors = str(i[6])
+    ifOutOctets = str(i[7])
+    ifOutUcastPkts = str(i[8])
+    ifOutNUcastPkts = str(i[9])
+    ifOutDiscards = str(i[10])
+    ifOutErrors = str(i[11])
+    ifInMulticastPkts = str(i[12])
+    ifInBroadcastPkts = str(i[13])
+    ifOutMulticastPkts = str(i[14])
+    ifOutBroadcastPkts = str(i[15])
 
+    rr = 'N:' + ifOperStatus
+    rr += ':' + ifInOctets
+    rr += ':' + ifInUcastPkts
+    rr += ':' + ifInNUcastPkts
+    rr += ':' + ifInDiscards
+    rr += ':' + ifInErrors
+    rr += ':' + ifOutOctets
+    rr += ':' + ifOutUcastPkts
+    rr += ':' + ifOutNUcastPkts
+    rr += ':' + ifOutDiscards
+    rr += ':' + ifOutErrors
+    rr += ':' + ifInMulticastPkts
+    rr += ':' + ifInBroadcastPkts
+    rr += ':' + ifOutMulticastPkts
+    rr += ':' + ifOutBroadcastPkts
 
-#netsnmp.Varbind('1.3.6.1.4.1.9.9.109.1.1.1.1.3.', ''))
-#'1.3.6.1.4.1.9.9.109.1.1.1.1.3.')
+    rrDict[ifName] = rr
+
+for k,v in rrDict.items():
+    #print(k)
+    #print(v)
+    rrdata = {'rrd': 'if.' + str(k), 'val': v}
+    rrList.append(rrdata)
+
+#print(json.dumps(rrDict, sort_keys=True, indent=4))
+
+json_data = '{ "system_id":"' + str(system_id) + '",'
+#json_data += '"rrdata": [' + str(json.dumps(json.loads(rrdata))) + ']'
+json_data += '"rrdata": ' + json.dumps(rrList)
+json_data += '}'
+print(json.dumps(json.loads(json_data), indent=4))
+
 
 # the function snmpwalk returns a set of 4-tuples:
 #   var.tag  (the object OID)
@@ -51,40 +91,5 @@ for i in returnList:
 
 #IF-MIB::ifOperStatus.2 = INTEGER: down(2)
 #IF-MIB::ifOperStatus.3 = INTEGER: up(1)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#session = netsnmp.Session(DestHost='216.34.200.142', Version=1, Community='public')
-#vars = netsnmp.VarList(netsnmp.Varbind('ifIndex',), netsnmp.Varbind('ifDescr',), netsnmp.Varbind('ifOperStatus',))
-#print(session.getbulk(0, 48, vars))
-
-#var = netsnmp.Varbind('sysDescr.0')
-#res = netsnmp.snmpget(var, Version = 1, DestHost = '216.34.200.142', Community='public')
-#print(res)
-
-#oid = netsnmp.Varbind('sysDescr')
-#result = netsnmp.snmpwalk(oid, Version = 1, DestHost="216.34.200.142",Community="public")
-#print(result)
-
-#var =  netsnmp.VarList(netsnmp.Varbind('ifDescr'))
-#var =  netsnmp.Varbind('ifDescr')
-#res = netsnmp.snmpget(var, Version = 1, DestHost = '216.34.200.142', Community='public')
-#for v in var:
-#    print(v)
-#print(res)
 
 
