c15c67a805672c6abbe4926a5fed4192a726830f
karl <karl.rink@nationsinfocorp.com>
2020-04-17 11:40:25 -0700

    sys/ipmi/collect.ipmi 01

--- sys/ipmi/collect.ipmi.py
+++ sys/ipmi/collect.ipmi.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2
 
-__version__ = '001'
+__version__ = '01'
 
 import sys
 sys.dont_write_bytecode = True
@@ -20,9 +20,6 @@ if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
     getattr(ssl, '_create_unverified_context', None)):
     ssl._create_default_https_context = ssl._create_unverified_context
 
-
-#import rrdtool
-
 def usage():
     print("Usage: " + sys.argv[0] + " host [option]" + """
 
@@ -40,9 +37,6 @@ def collect_ipmi(host):
     exit_code = p.wait()
     if (exit_code != 0):
         print('Error: ' + str(err) + ' ' + str(output))
-        #return chronyc_data, chronyc_alert
-        #return False
-        #sys.exit(exit_code)
         return {}
 
     multilines = output.splitlines()
@@ -51,12 +45,9 @@ def collect_ipmi(host):
     for line in multilines:
         line = line.split('|')
         #print(line)
-        #column0 = line[0]
-        #print(len(line)) #3
         column0 = line[0]
         column1 = line[1]
         column2 = line[2]
-
         #print(column0, column1, column2)
 
         column0 = column0.strip()
@@ -68,34 +59,14 @@ def collect_ipmi(host):
         column0 = column0.replace('+', '')
         column0 = column0.replace('.', '-')
 
-        #print(len(column0))
-        #print(column0)
-
         #Note: A ds-name must be 1 to 19 characters [a-zA-Z0-9_]
         if len(column0) > 19:
             column0 = column0[:19]
 
-        #print column0
-
         column1 = column1.strip()
-        #print(column0 + ' ' + column1)
-
         column2 = column2.strip()
-        #print(column0 + ' ' + column2)
-
-        #if column2 == 'ns':
-        #    print(column0 + ' ' + column2)
-        #if column2 != 'ok':
-        #    print(column0 + ' ' + column2)
-
-        #print(column0 + '|' + column1 + '|' + column2)
 
         column1 = column1.split(' ')
-
-        #if column1[1]:
-        #    if column1[1] == 'Watts':
-        #        column0 = column0 + 'Watts'
-
         try:
             if column1[1] == 'CFM':
                 column0 = column0 + '_CFM'
@@ -106,8 +77,6 @@ def collect_ipmi(host):
             #if column1[1] == 'Volts':
             #    column0 = column0 + 'Volts'
             if column1[1] == 'degrees':
-                #column0 = column0 + column1[2]
-                #column0 = column0 + 'Celsius'
                 column0 = column0 + '_C'
 
         except IndexError as e:
@@ -118,16 +87,7 @@ def collect_ipmi(host):
 
         DDict[column0] = column1[0]
 
-
-    #for k,v in DDict.items():
-    #    if v == '0x00':
-    #        continue
-    #    if v == 'no reading':
-    #        continue
-    #    print(v)
-
     for k,v in DDict.items():
-        #print('VAL - ' + v)
         if v == '0x00':
             del DDict[k]
         #if v == 'no reading':
@@ -136,7 +96,6 @@ def collect_ipmi(host):
         if v == 'disabled':
             del DDict[k]
 
-    #print(DDict)
     return DDict
 
 def ipmiRRD(rrdfile, data_dict):
@@ -190,14 +149,9 @@ def post(system_id, json_data):
         request.add_header('x-api-key', system_id)
         post_data = json.dumps(json.JSONDecoder().decode(json_data))
         response = urllib2.urlopen(request, post_data, timeout=20)
-        #print(response.read())
         return response.read()
     except Exception as e:
-        #print('HTTP Post error: ' + str(e))
         return str(e)
-
-
-
 
 if __name__ == "__main__":
     disable_post = write_local = False
@@ -218,14 +172,8 @@ if __name__ == "__main__":
         sys.exit(1)
 
     c = collect_ipmi(host)
-    #hdr = 'N'
-    #val = 'N'
-    #for k,v in c.items():
-    #    hdr += ':' + str(k)
-    #    val += ':' + str(v)
 
     rrd = {'rrd': 'ipmi', 'val': c, 'type': 'json'}
-    #rrd = {'rrd': 'ipmi.json', 'val': c} 
     rrdList = [ rrd ]
 
     json_data  = '{ "system_id": "' + str(system_id) + '",'
@@ -246,40 +194,4 @@ if __name__ == "__main__":
         except Exception as e:
             print(response)
 
-    sys.exit(1)
-
-#    for host, system_id in config.hosts.items():
-#        #print(system_id)
-#        c = collect_ipmi(host)
-#        
-#        #print(c)
-#        #sys.exit(99)
-#
-#        val = 'N'
-#        rrdfile = str(host) + '.rrd'
-#        if not os.path.isfile(rrdfile):
-#            ipmiRRD(rrdfile, c)
-#        else:
-#            for k,v in c.items():
-#                val += ':' + str(v)
-#
-#            try:
-#                rrdtool.update(str(rrdfile), str(val))
-#            except Exception as e:
-#                print(e)
-#
-#        #rrd = {'rrd': str(domain.name()) + '.' + str(item), 'val': rrd_val, 'type': 'ipmi.json'}
-#        rrd = {'rrd': 'ipmi.json', 'val': val, 'type': 'ipmi.json'}
-#        #print(rrd)
-#
-#        rrdList = [rrd]
-#
-#        #system_id = '1234'
-#
-#        json_data  = '{ "system_id": "' + str(system_id) + '",'
-#        json_data += '"rrdata": ' + str(json.dumps(rrdList))
-#        json_data += '}'
-#
-#        print(json.dumps(json.loads(json_data), sort_keys=True, indent=4))
-
-
+    sys.exit(0)
