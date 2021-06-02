de6fe59c04bee0246cff933b26925aa528d49fed
Karl Rink <karl@rink.us>
2021-06-01 19:39:14 -0700

    🌈 1.1.1

--- setup.py
+++ setup.py
@@ -14,13 +14,13 @@ setup(
     entry_points = {
         "console_scripts": ['jumpcloud = jumpcloud.jumpcloud:main']
         },
-    version = '1.1.0-1',
+    version = '1.1.1',
     description = "jumpcloud command for jumpcloud.com",
     long_description = "Python command line tool for administration of jumpcloud.com api.",
     author = "Karl Rink",
     author_email = "karl@rink.us",
     url = "https://gitlab.com/krink/jumpcloud",
-    install_requires = [ 'urllib3', 'json' ]
+    install_requires = [ 'urllib3', ]
     )
 
 
