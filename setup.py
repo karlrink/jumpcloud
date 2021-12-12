8e556181eb44960e41d7c29b9ffac1bfcd68435d
Karl Rink <karl@rink.us>
2021-12-12 13:51:59 -0800

    🎄 2.0.0

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ from setuptools import setup
 
 setup(
     name = "jumpcloud",
-    packages = ["jumpcloud"],
+    packages = ["jumpcloud","jumpcloud.reports"],
     entry_points = {
         "console_scripts": ['jumpcloud = jumpcloud.jumpcloud:main']
         },
-    version = '2.0-0',
+    version = '2.0.0',
     description = "jumpcloud command for jumpcloud.com",
     long_description = "Python command line tool for administration of jumpcloud.com api.",
     author = "Karl Rink",
--- tests/qa/qa2.sh
+++
@@ -1,8 +0,0 @@
-
-#mac
-../src/jumpcloud/jumpcloud.py get-app Jumpcloud
-
-#win
-../src/jumpcloud/jumpcloud.py get-program "JumpCloud v0.235.1"
-
-
